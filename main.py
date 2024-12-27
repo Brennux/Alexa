29ba305b72f4733af62d50e964497d8cdd157e5a
Brennux <nascimentobreno730@gmail.com>
2024-12-27 17:19:01 -0300

    .

---
+++ main.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import pyttsx3
+
+
+Listener = sr.Recognizer()
+engine = pyttsx3.init()
+engine.say('Eu Sou sua Alexa')
+engine.say('O que posso fazer Por você')
+engine.runAndWait()
+
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = Listener.listen(source)
+        command = Listener.Recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
+except: 
+    pass
